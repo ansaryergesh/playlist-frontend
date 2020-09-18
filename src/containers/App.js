e64791388c42dcecae01c47aa99608aa7beedc5f
ansaryergesh <yergeshovansar@gmail.com>
2020-09-18 19:58:23 +0600

    fetching musics

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import Toggle from './Toggle';
-import { ConfigureStore } from '../store/configureStore';
 
+const App = () => {
+  const [musics, setMusics] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const[currentPage, setCurrentPage] = useState(1);
+  const[musicPerPage, setMusicPerPage] = useState(10);
 
-const store = ConfigureStore();
-class App extends Component {
- 
-  render() {
-    return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <div className="App">
-            <Toggle />
-          </div>
-        </BrowserRouter>
-      </Provider>
+  useEffect(() => {
+    const fetchMusics = async () => {
+      setLoading(true);
+      const result = await axios.get('http://localhost:8000/api/musics');
+      setMusics(result.data);
+      setLoading(false)
+    }
 
-    );
-  }
+    fetchMusics();
+  }, []);
+
+  console.log(musics)
+  return (
+    <div className = 'container'>
+      <h1>Playlist</h1>
+    </div>
+  )
 }
 export default App;
